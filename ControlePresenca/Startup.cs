5873c4b09073b98a4573509aad18a5c762548b90
Mariana Souza <Mari_rodrigue@hotmail.com>
2023-03-19 22:57:29 -0300

    configura CORS

--- ControlePresenca/Startup.cs
+++ ControlePresenca/Startup.cs
@@ -34,6 +34,8 @@ namespace ControlePresenca
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(t => t.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
